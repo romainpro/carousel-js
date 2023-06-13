42a60ceb6bc319b60ed02a44c525feb559d72d8a
romainpro <romain.casagrande.pro@gmail.com>
2023-06-13 14:20:15 +0200

    format-js

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-const arrayImg = ['media/img1.jpg', 'media/img2.jpg', 'media/img3.jpg', 'media/img4.jpg'];
-const btnNext = document.getElementById('btnNext');
-const btnPrevious = document.getElementById('btnPrevious');
-const carousel = document.querySelector('.carousel');
+const arrayImg = [
+  "media/img1.jpg",
+  "media/img2.jpg",
+  "media/img3.jpg",
+  "media/img4.jpg",
+];
 
-btnNext.addEventListener('click', nextImage);
-btnPrevious.addEventListener('click', previousImage);
+const btnNext = document.getElementById("btnNext");
+const btnPrevious = document.getElementById("btnPrevious");
+const carousel = document.querySelector(".carousel");
+
+btnNext.addEventListener("click", nextImage);
+btnPrevious.addEventListener("click", previousImage);
 
 let currentIndex = 0;
-let img = document.createElement('img');
+let img = document.createElement("img");
 img.src = arrayImg[currentIndex];
 carousel.appendChild(img);
 
